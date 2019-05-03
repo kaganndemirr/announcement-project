4604b49966b6fff04cfa57936da8b013ffedf39d
kaganndemirr <kaganndemirr@gmail.com>
2019-05-03 16:23:06 +0300

    desired json format

--- announcement/views.py
+++ announcement/views.py
@@ -38,34 +38,102 @@ class MainView(TemplateView):
 
 class AjaxContents(View):
     def get(self, request):
-        data = Content.objects.filter(department=request.user.department,
-            status=0).order_by('creation_time').values()
+        data = [
+            {
+                'title': i['title'],
+                'content': i['data'],
+                'type': i['type']
+            }
+
+            for i in Content
+                    .objects
+                    .filter(department=request.user.department)
+                    .order_by('-creation_time').values()
+        ]
         return JsonResponse({"contents": list(data)})
 
 
 class AjaxAnnouncement(View):
     def get(self, request):
-        data = Announcement.objects.filter(department=request.user.department).values()
+        data = [
+            i['title']
+            for i in Announcement
+                    .objects
+                    .filter(department=request.user.department)
+                    .order_by('creation_date').values()
+        ]
         return JsonResponse({"announcements": list(data)})
 
 
 class AjaxLectures(View):
     def get(self, request):
-        data = Lecture.objects.filter(department=request.user.department,
-            l_date__date=date.today() + timedelta(days=1)).order_by('l_date').values()
+        data = [
+            {
+                'code': i['l_code'],
+                'name': i['title'],
+                'lecturer': i['lecturer'],
+                'time': j.l_date.time(),
+            }
+
+            for j in Lecture
+                    .objects
+                    .filter(department=request.user.department,
+                        l_date__date=date.today())
+                    .order_by('l_date')
+
+            for i in Lecture
+                    .objects
+                    .filter(department=request.user.department,
+                        l_date__date=date.today())
+                    .order_by('l_date').values()
+        ]
         return JsonResponse({"lectures": list(data)})
+
 
 class AjaxExams(View):
     def get(self, request):
-        data = Exam.objects.filter(lecture__department=request.user.department,
-            e_date__date=date.today() + timedelta(days=1)).order_by('e_date').values()
+        data = [
+            {
+                'code': j.lecture.l_code,
+                'name': j.lecture.title,
+                'lecturer': j.lecture.lecturer,
+                'datetime': i['e_date'],
+                'location': i['location'],
+                'duration': i['duration']
+            }
+
+            for j in Exam
+                    .objects
+                    .filter(lecture__department=request.user.department,
+                        e_date__date__gt=date.today())
+                    .order_by('e_date')
+
+            for i in Exam
+                    .objects
+                    .filter(lecture__department=request.user.department,
+                        e_date__date=date.today())
+                    .order_by('e_date').values()
+        ]
         return JsonResponse({"exams": list(data)})
+
 
 class AjaxEvents(View):
     def get(self, request):
-        data = Event.objects.filter(department=request.user.department,
-            date__date=date.today() + timedelta(days=1)).order_by('date').values()
+        data = [
+            {
+                'date': i['date'],
+                'name': i['name'],
+                'location': i['location']
+            }
+
+            for i in Event
+                    .objects
+                    .filter(department=request.user.department,
+                        date__date__gte=date.today())
+                    .order_by('date').values()
+        ]
         return JsonResponse({"events": list(data)})
+
 
 class AjaxWeather(View):
     def get(self, request):
