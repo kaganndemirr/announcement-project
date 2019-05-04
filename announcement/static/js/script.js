b4fc223863757076e8427e0403f136b729e3d91d
webninjasi <webninjasi.0@gmail.com>
2019-05-04 15:55:09 +0300

    Refactor scripts

--- announcement/static/js/script.js
+++ announcement/static/js/script.js
@@ -1,7 +1,25 @@
-function init() {
-  $.get("/ajax/weather", function(weather) {
-    $('#weather_forecast').text(weather.Trabzon);
-  });
-}
+// Scroll Text
+(function(window) {
+  var speedFactor = 10;
 
-init();
+  $("#scroll_speedup").click(function() {
+    if (speedFactor > 1) {
+      speedFactor --;
+    }
+
+    $("#scroll_text").css('animation', "scroll " + speedFactor + "s linear infinite");
+  });
+
+  $("#scroll_speeddown").click(function() {
+    speedFactor ++;
+
+    $("#scroll_text").css('animation', "scroll " + speedFactor + "s linear infinite");
+  });
+})(window);
+
+// Clock
+(function(window) {
+  setInterval(function() {
+    $("#tarihsaat").html(new Date().toLocaleString());
+  }, 1000);
+})(window);
