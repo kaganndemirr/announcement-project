3290e8e6a403663fb86d53a3daec507322eaeb7e
webninjasi <webninjasi.0@gmail.com>
2019-05-05 16:42:59 +0300

    Fix utcoffset error

--- announcement/apps/classes/models.py
+++ announcement/apps/classes/models.py
@@ -1,5 +1,5 @@
 # Standart Library
-from datetime import date
+from datetime import datetime
 
 # Django
 from django.db import models
@@ -14,7 +14,7 @@ class Lecture(models.Model):
     l_code = models.CharField(verbose_name='Code', max_length=10, unique=True)
     title = models.CharField(verbose_name='Lecture Name', max_length=30)
     lecturer = models.CharField(verbose_name='Lecturer Name', max_length=30)
-    l_date = models.DateTimeField(verbose_name=_('Lecture Date '), default=date.today)
+    l_date = models.DateTimeField(verbose_name=_('Lecture Date '), default=datetime.now)
 
     class Meta:
         verbose_name = _('Lecture')
@@ -26,7 +26,7 @@ class Lecture(models.Model):
 
 class Exam(models.Model):
     lecture = models.ForeignKey(to='classes.Lecture', on_delete=models.CASCADE)
-    e_date = models.DateTimeField(verbose_name=_('Exam Date'), default=date.today)
+    e_date = models.DateTimeField(verbose_name=_('Exam Date'), default=datetime.now)
     duration = models.CharField(verbose_name='Duration', max_length=10)
     location = models.CharField(verbose_name='Location', max_length=50, blank=True)
 
